3f3e145d232ab5940541d279e08c100ba8e6dd1f
Łukasz Bolechów <lukaszbolechow@gmail.com>
2024-10-10 02:47:06 +0200

    Poprawka w klasie JwtRequestFilter.

--- src/main/java/pl/lukbol/ProjektSkillforge/Configs/JwtRequestFilter.java
+++ src/main/java/pl/lukbol/ProjektSkillforge/Configs/JwtRequestFilter.java
@@ -20,13 +20,12 @@ import java.io.IOException;
 public class JwtRequestFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     public JwtRequestFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
-
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
@@ -42,7 +41,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         }
 
         if (usernameOrEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(usernameOrEmail);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(usernameOrEmail);
             if (jwtUtil.validateToken(jwt, userDetails.getUsername())) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
